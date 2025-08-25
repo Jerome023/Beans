8a18c6311efb21f314a738919486950e80ae0c31
lerc0005-rgb <lerc0005@asms.sa.edu.au>
2025-08-25 11:32:55 +0930

    Update Plant Lists.py

--- Plant Lists.py
+++ Plant Lists.py
@@ -1,5 +1,13 @@
 <<<<<<< HEAD
 Test
 =======
+import pygame
 
 >>>>>>> 5a593283b57a1b87a919a3156ade31c1d05fadee
+pygame.init()
+
+screen_width = 360
+screen_height = 650
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Plant Lists")
+colour = (0, 0, 0)
